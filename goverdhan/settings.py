b9751d36636890f80827f0045461b04365725feb
keval vavaliya <72963356+kevalvavaliya@users.noreply.github.com>
2022-01-11 13:12:22 +0530

    Update settings.py

--- goverdhan/settings.py
+++ goverdhan/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'goverdhan')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'goverdhan/static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
